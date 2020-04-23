c263bf42abd9f8d7d39d911989ed4690e4123687
Ayuth Mangmesap <ayuth.mang@gmail.com>
2020-04-23 10:28:14 +0700

    add example for fragment

---
+++ src/playground/fragment/FragmentExample.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hello = (props) => <h1>Hi, {props.name}</h1>;
+
+const App = () => (
+  <>
+    <Hello name={"John"} />
+    <Hello name={"Jane"} />
+    <Hello name={"Jim"} />
+  </>
+);
+
+export default App;
