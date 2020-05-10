5e1d5d46d31c71e347485540aa05a3a14fe5add2
Ayuth Mangmesap <ayuth.mang@gmail.com>
2020-05-11 01:11:28 +0700

    re-arrange code

--- src/playground/state/Counter.test.js
+++ src/playground/state/Counter.test.js
@@ -3,6 +3,16 @@ import { render, fireEvent } from '@testing-library/react'
 
 import Counter from './Counter'
 
+test('given count is 3, when pass count as props, then it should renders 3', () => {
+  const { getByText } = render(<Counter count={3} />)
+  expect(getByText('Count: 3')).toBeInTheDocument()
+})
+
+test('when props is not passed, then it should renders default count should be 0', () => {
+  const { getByText } = render(<Counter />)
+  expect(getByText('Count: 0')).toBeInTheDocument()
+})
+
 test('when clicks increase button, then it should increased', () => {
   const { getByText } = render(<Counter />)
 
@@ -29,15 +39,5 @@ test('when clicks reset, then it should reset', () => {
   const reset = getByText('reset')
   fireEvent.click(reset)
 
-  expect(getByText('Count: 0')).toBeInTheDocument()
-})
-
-test('given count is 3, when pass count as props, then it should renders 3', () => {
-  const { getByText } = render(<Counter count={3} />)
-  expect(getByText('Count: 3')).toBeInTheDocument()
-})
-
-test('when props is not passed, then it should renders default count should be 0', () => {
-  const { getByText } = render(<Counter />)
   expect(getByText('Count: 0')).toBeInTheDocument()
 })
